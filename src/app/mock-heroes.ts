74bad397ef2297b28d8f23f2b93187edd9076db2
Smit Rao <smit.rao@soti.net>
2019-05-15 10:15:38 -0400

    Adding service

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,16 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'WomannN M<AN <3+' },
+  { id: 12, name: 'WomannN M<AN <3++' },
+  { id: 13, name: 'mR. mAN Dude Dawg' },
+  { id: 14, name: 'Guy Dude Man Dawgg' },
+  { id: 15, name: 'NMc Donaldz' },
+  { id: '4^2', name: 'WomannN M<AN <3+-' },
+  { id: 17, name: 'Boy Girl MAn' },
+  { id: 18, name: 'InsightHQ' },
+  { id: 19, name: 'Benefits Boy' },
+  { id: 20, name: 'WomannN M<AN <3+' },
+  { id: '9+10', name: 'Idiot Man' }
+
 ];
