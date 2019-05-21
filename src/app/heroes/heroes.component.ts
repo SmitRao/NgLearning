efc9a9a5227915731a99ff8475c5dd2b9b3d64c6
Smit Rao <smit.rao@mail.utoronto.ca>
2019-05-21 00:36:28 -0400

    Add navigation features

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) {
     // constructor apparently should not do anything
@@ -20,12 +19,11 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
